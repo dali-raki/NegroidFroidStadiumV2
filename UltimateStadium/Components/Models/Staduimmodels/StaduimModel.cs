92090621ef7a4a2760dc1a7221fc06aa116f5a17
Dali_Raki <Dali_Raki@DESKTOP-KJDD8OP>
2024-11-23 22:15:25 +0100

    `ADD Login &  search staduim`

---
+++ UltimateStadium/Components/Models/Staduimmodels/StaduimModel.cs
@@ -0,0 +1,12 @@
+﻿namespace UltimateStadium.Components.Models.Staduimmodels
+{
+    public class StaduimModel
+    {
+
+        public string stadiumId { get; set; }
+        public string stadiumName { get; set; }
+        public string stadiumPlace { get; set; }
+        public double stadiumRentalPrice { get; set; }
+        //public bool isRented { get; set; }
+    }
+}
