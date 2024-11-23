1b105b9022cd428d311a4567fee704edce2c928e
bunny <haithemkahoul1@gmail.com>
2024-11-23 20:40:15 +0100

    2

---
+++ UltimateStadium/Domain/Stadium.cs
@@ -0,0 +1,10 @@
+namespace BlazorApp1.Domain;
+
+public class Stadium
+{
+    public string stadiumId { get; set; }
+    public string stadiumName { get; set; }
+    public string stadiumPlace { get; set; }
+    public double stadiumRentalPrice { get; set; }
+    public bool isRented { get; set; }
+}
