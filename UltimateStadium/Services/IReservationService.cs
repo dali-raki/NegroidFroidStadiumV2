79ef3ac5f680db7a5d7cde37370326d2855a2eb4
ghendirmabroukbenalia@gmail.com <ghendirmabroukbenalia@gmail.com>
2024-12-06 22:59:11 +0100

    Reservation service

---
+++ UltimateStadium/Services/IReservationService.cs
@@ -0,0 +1,14 @@
+﻿using UltimateStadium.Domain;
+
+namespace UltimateStadium.Services;
+
+public interface IReservationService
+{
+    Task<bool> InsertReservation(Reservation reservation);
+    Task<Reservation> SelectReservationById(string reservationId);
+    Task<List<Reservation>> SelectAllReservations();
+    Task<bool> UpdateReservation(Reservation reservation);
+    Task<bool> DeleteReservation(string reservationId);
+    Task<int> GetTotalReservationsByStadiumAndMonth(string stadiumId, int year, int month);
+    Task<int> GetTotalReservationsByMonthAndYearAsync(int year, int month);
+}
---
+++ UltimateStadium/Services/ReservationService.cs
@@ -0,0 +1,105 @@
+﻿using UltimateStadium.Domain;
+using UltimateStadium.Storage.ReservationStorage;
+
+namespace UltimateStadium.Services;
+
+public class ReservationService : IReservationService
+{
+    private readonly IReservationStorage reservationStorage;
+
+    public ReservationService(IReservationStorage reservationStorage)
+    {
+        this.reservationStorage = reservationStorage;
+    }
+
+    public async Task<bool> InsertReservation(Reservation reservation)
+    {
+        try
+        {
+            return await reservationStorage.InsertReservation(reservation);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
+        }
+    }
+
+    public async Task<Reservation> SelectReservationById(string reservationId)
+    {
+        try
+        {
+            return await reservationStorage.SelectReservationById(reservationId);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
+        }
+    }
+
+    public async Task<List<Reservation>> SelectAllReservations()
+    {
+        try
+        {
+            return await reservationStorage.SelectAllReservations();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
+        }
+    }
+
+    public async Task<bool> UpdateReservation(Reservation reservation)
+    {
+        try
+        {
+            return await reservationStorage.UpdateReservation(reservation);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
+        }
+    }
+
+    public async Task<bool> DeleteReservation(string reservationId)
+    {
+        try
+        {
+            return await reservationStorage.DeleteReservation(reservationId);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
+        }
+    }
+
+    public async Task<int> GetTotalReservationsByStadiumAndMonth(string stadiumId, int year, int month)
+    {
+        try
+        {
+            return await reservationStorage.GetTotalReservationsByStadiumAndMonth(stadiumId, year, month);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
+        }
+    }
+
+    public async Task<int> GetTotalReservationsByMonthAndYearAsync(int year, int month)
+    {
+        try
+        {
+            return await reservationStorage.GetTotalReservationsByMonthAndYearAsync(year, month);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
+        }
+    }
+}
